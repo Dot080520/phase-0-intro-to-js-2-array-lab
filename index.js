3c37f1d59d22cfc9fbe37ec45d094721fbbd89cc
Michael Lombard <miklombard@yahoo.com>
2023-05-04 01:24:01 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Write your solution here!
+// Write your solution her
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+function appendCat(name) {
+    const copyOfCats = [...cats];
+    copyOfCats.push(name);
+    return copyOfCats;
+}
+function prependCat(name) {
+    const allCats = [...cats];
+    allCats.unshift(name);
+    return allCats;
+}
+function removeLastCat() {
+    const lessCats = [...cats];
+    lessCats.pop();
+    return lessCats;
+}
+function removeFirstCat() {
+    const firstCat = [...cats];
+    firstCat.shift();
+    return firstCat;
+}
